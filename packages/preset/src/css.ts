8985ef1f9a681bf637c44b59774fe30334b000df
zhaogongchengsi <zzh1586169624@163.com>
2023-08-16 11:46:53 +0800

    fix: Fix bug with color failure

--- packages/preset/src/css.ts
+++ packages/preset/src/css.ts
@@ -2,5 +2,7 @@ import {PresetVedixOptions} from "./index";
 import {generateColor} from "./colors";
 
 export  function genCss (options: PresetVedixOptions) {
-    return generateColor(options)
+    return generateColor(options, (color) => {
+        return color.split(" ").join(", ")
+    })
 }
