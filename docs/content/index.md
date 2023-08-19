fe0cd40c4eef829fb1f9a521c760b6709f766d08
zhaogongchengsi <zzh1586169624@163.com>
2023-08-19 15:22:17 +0800

    feat: add docs

---
+++ docs/content/index.md
@@ -0,0 +1,20 @@
+::block-hero
+---
+cta:
+  - Get started
+  - /get-started:
+secondary:
+  - Open on GitHub →
+  - https://github.com/nuxtlabs/docus
+snippet: 
+  - npx nuxi@latest 
+  - init docus-app -t 
+  - nuxtlabs/docus-starter
+---
+
+#title
+Document-driven framework
+
+#description
+Docus reconciles content creators and developers by offering to both the best tools to create and scale content-based websites.
+::
---
+++ docs/nuxt.config.ts
@@ -0,0 +1,62 @@
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+  ],
+
+  docus: {
+    title: 'Vedix',
+    description: 'Vedix UI',
+  },
+
+  extends: '@nuxt-themes/docus',
+
+  experimental: {
+    payloadExtraction: false,
+    inlineSSRStyles: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
+
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    prerender: {
+      crawlLinks: false,
+      routes: ['/'],
+      ignore: ['/hi'],
+    },
+  },
+
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ],
+    },
+  },
+
+  devtools: {
+    enabled: true,
+  },
+})
