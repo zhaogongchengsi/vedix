b98835c239736f2a020ce221a82bb0a81e2fa5ce
zhaogongchengsi <zzh1586169624@163.com>
2023-08-24 16:40:55 +0800

    change: add change components

--- packages/components/src/avatar/avatar.vue
+++ packages/components/src/avatar/avatar.vue
@@ -1,42 +1,25 @@
 <script setup lang='ts'>
 import { OkuAvatar, OkuAvatarFallback, OkuAvatarImage } from '@oku-ui/avatar'
-import { cva } from 'class-variance-authority'
 
 defineOptions({
   name: 'Avatar',
   inheritAttrs: true,
 })
 
-withDefaults(defineProps<AvatarProps>(), {
+withDefaults(defineProps<{ delayMs?: number; size?: 'default' | 'sm' | 'lg'; src: string }>(), {
   delayMs: 300,
   size: 'default',
 })
-
-interface AvatarProps {
-  src?: string
-  delayMs?: number | string
-  size?: 'default' | 'sm' | 'lg'
-}
-
-const avatarVariants = cva(
-  'block rounded-full overflow-hidden',
-  {
-    variants: {
-      size: {
-        default: 'h-9 w-9',
-        sm: 'h-8 w-8',
-        lg: 'h-10 w-10',
-      },
-    },
-    defaultVariants: {
-      size: 'default',
-    },
-  },
-)
 </script>
 
 <template>
-  <OkuAvatar :class="avatarVariants({ size })">
+  <OkuAvatar
+    class="block rounded-full overflow-hidden" :class="{
+      'h-9 w-9': size === 'default',
+      'h-8 w-8': size === 'sm',
+      'h-10 w-10': size === 'lg',
+    }"
+  >
     <slot>
       <OkuAvatarImage :src="src" />
     </slot>
--- packages/components/src/button/button.vue
+++ packages/components/src/button/button.vue
@@ -15,20 +15,18 @@ withDefaults(
 
 <template>
   <button
-    class="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" :class="[
-      {
-        'bg-primary text-primary-foreground shadow hover:bg-primary/90': variant === 'default',
-        'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90': variant === 'destructive',
-        'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground': variant === 'outline',
-        'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80': variant === 'secondary',
-        'hover:bg-accent hover:text-accent-foreground': variant === 'ghost',
-        'text-primary underline-offset-4 hover:underline': variant === 'link',
-        'h-9 px-4 py-2': size === 'default',
-        'h-8 rounded-md px-3 text-xs': size === 'sm',
-        'h-10 rounded-md px-8': size === 'lg',
-        'h-9 w-9': size === 'icon',
-      },
-    ]"
+    class="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" :class="{
+      'bg-primary text-primary-foreground shadow hover:bg-primary/90': variant === 'default',
+      'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90': variant === 'destructive',
+      'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground': variant === 'outline',
+      'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80': variant === 'secondary',
+      'hover:bg-accent hover:text-accent-foreground': variant === 'ghost',
+      'text-primary underline-offset-4 hover:underline': variant === 'link',
+      'h-9 px-4 py-2': size === 'default',
+      'h-8 rounded-md px-3 text-xs': size === 'sm',
+      'h-10 rounded-md px-8': size === 'lg',
+      'h-9 w-9': size === 'icon',
+    }"
   >
     <slot />
   </button>
