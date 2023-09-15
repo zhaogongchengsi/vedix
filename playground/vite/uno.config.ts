b2d43d9d61fb796ecde50e12d8f51977984071e1
zhaogongchengsi <zzh1586169624@163.com>
2023-09-15 17:41:06 +0800

    change: change unocss.config

--- playground/vite/uno.config.ts
+++ playground/vite/uno.config.ts
@@ -5,12 +5,15 @@ import {
   presetUno,
   presetWebFonts,
 } from 'unocss'
-import parseVedix from 'vedix-unocss-preset'
+import { presetVedix, slate } from 'vedix-unocss-preset'
 
 export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600 !outline-none'],
+  ],
+  rules: [
+    [/^size-(\d+)$/, ([, d]) => ({ width: `${d}rem`, height: `${d}rem` })],
   ],
   presets: [
     presetUno(),
@@ -26,6 +29,6 @@ export default defineConfig({
         mono: 'DM Mono',
       },
     }),
-    parseVedix(),
+    presetVedix({ theme: slate }),
   ],
 })
